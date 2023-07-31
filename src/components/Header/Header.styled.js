1816277c21fb9b37b7089bc10ba89fc8228ae531
OksanaGerzanych <109728377+OksanaGerzanych@users.noreply.github.com>
2023-07-31 21:59:43 +0300

    header

---
+++ src/components/Header/Header.styled.js
@@ -0,0 +1,76 @@
+import styled from "styled-components";
+
+export const HeaderStyle = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid rgba(35, 31, 32, 0.25);
+  padding-bottom: 40px;
+  padding-top: 40px;
+`;
+
+export const ContainerLogo = styled.div`
+  display: flex;
+  gap: 17px;
+`;
+
+export const Logo = styled.img`
+  display: flex;
+  width: 65px;
+  height: 55px;
+`;
+
+export const LogoText = styled.p`
+  color: #a19fa1;
+  font-family: "Nunito", sans-serif;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+export const ContainerName = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ContainerCart = styled.div`
+  width: 150px;
+  height: 50px;
+  border-radius: 30px;
+  background: #f26b25;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+`;
+
+export const CartPrice = styled.p`
+  /* position: "relative"; */
+  font-family: "Nunito", sans-serif;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  margin-right: 10px;
+  /* &::after {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: 100%;
+    margin-left: 5px;
+    transform: translateY(-50%);
+    height: 80%;
+    border: 1px solid rgba(35, 31, 32, 255);
+  } */
+`;
+
+export const CartNumber = styled.p`
+  font-family: "Nunito", sans-serif;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  margin-left: 5px;
+`;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,24 @@
+import logo from "../../img/pizza-logo.png";
+import {BsCart4} from 'react-icons/bs'
+import { HeaderStyle, ContainerLogo, Logo, ContainerName, LogoText, ContainerCart, CartPrice, CartNumber } from "./Header.styled.js";
+
+export default function Header() {
+  return (
+    <HeaderStyle>
+      <ContainerLogo>
+        <Logo src={logo} alt="logo" />
+        <ContainerName>
+          <h1>PIZZA</h1>
+          <LogoText>the best pizza of the world</LogoText>
+        </ContainerName>
+      </ContainerLogo>
+      <ContainerCart>
+        <a href="">
+          <CartPrice>210grn</CartPrice>   
+          <BsCart4 />
+          <CartNumber>3</CartNumber>
+        </a>
+      </ContainerCart>
+    </HeaderStyle>
+  );
+}
